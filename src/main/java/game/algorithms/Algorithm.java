bc93baee9c1e79ce17c86c32968b3db2d87b3d76
nmrlo-iscteiul <nmrlo@iscte-iul.pt>
2019-11-01 00:38:56 +0000

    files to see graphs/gaussian truncated/lightly tested

--- src/main/java/game/algorithms/Algorithm.java
+++ src/main/java/game/algorithms/Algorithm.java
@@ -3,7 +3,7 @@ package game.algorithms;
 public class Algorithm {
 
 	//For n cases return a random n
-	public int randUniform (int numberCases) {
+	public static int randUniform (int numberCases) {
 		double u = Math.random();
 		for (int i=1; i<=numberCases; i++)
 			if (u <= (1.0/(double)numberCases)*(double)i)
@@ -12,7 +12,7 @@ public class Algorithm {
 	}
 
 	// 50% Common out-1 // 30% Rare out-2 // 15% Epic out-3 // 5% Legendary out-4 //
-	public int randDropClass () {
+	public static int randDropClass () {
 		double u = Math.random();
 		if (u <= 0.5) return 1;
 		else if (u <= 0.8) return 2;
@@ -21,14 +21,14 @@ public class Algorithm {
 	}
 
 	// 70% Normal out-1 // 30% Strong out-2 //
-	public int randMobClass () {
+	public static int randMobClass () {
 		double u = Math.random();
 		if (u <= 0.7) return 1;
 		else return 2;
 	}
 
 	// mob attack timer
-	public double triangle () {
+	public static double triangle () {
 		return Math.sqrt(25*Math.random())+7;
 	}
 
@@ -40,19 +40,22 @@ public class Algorithm {
 	
 	//generate variables with normal distribution with media and variance
 	
-	public double uniform (int inf, int sup) {
+	public static double uniform (int inf, int sup) {
 		return (sup-inf)*Math.random()+inf;
 	}
 	
-	public double normal( double media, double var ) {
-		assert(var > 0);
-		double p, p1, p2;
+	public static double normal( double media, double var, double ddown, double dup ) {
+		assert(var > 0 && ddown < dup);
+		double p, p1, p2, x;
 		do {
-			p1 = uniform(-1, 1);
-			p2 = uniform(-1, 1);
-			p = p1 * p1 + p2 * p2;
-		} while ( p >= 1. );
-		return media + var * p1 * Math.sqrt(-2 * Math.log(p) / p);
+			do {
+				p1 = uniform(-1, 1);
+				p2 = uniform(-1, 1);
+				p = p1 * p1 + p2 * p2;
+			} while ( p >= 1. );
+			x = media + var * p1 * Math.sqrt(-2 * Math.log(p) / p);
+		} while ( x < ddown || x > dup);
+		return x;
 	}
 
 
